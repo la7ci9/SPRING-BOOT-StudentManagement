02a4de0024290e07bece972c1dce4baa6d643c74
la7ci9 <akarmiis997@gmail.com>
2022-06-16 10:30:04 +0200

    Created saveStudent and creatNewStudent methods

--- src/main/java/com/example/management/controll/StudentControll.java
+++ src/main/java/com/example/management/controll/StudentControll.java
@@ -4,7 +4,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
+import com.example.management.entity.Student;
 import com.example.management.service.StudentService;
 
 @Controller
@@ -21,7 +24,18 @@ public class StudentControll {
 	public String listStudents(Model model) {
 		model.addAttribute("students",studentService.getAllStudents());
 		return "students";
-		
 	}
 	
+	@GetMapping("/students/new")
+	public String createNewStudent(Model model) {
+		Student student = new Student();
+		model.addAttribute("student",student);
+		return "create_student";
+	}
+	
+	@PostMapping("/students")
+	public String saveStudent(@ModelAttribute("student") Student student) {
+		studentService.saveStudent(student);
+		return "redirect:/students";
+	}
 }
