eaa2ab36411d6df4e715f75bb380d470af930e8c
la7ci9 <akarmiis997@gmail.com>
2022-06-16 10:26:17 +0200

    Comment deleted

--- src/main/java/com/example/management/StudentManagementSystemApplication.java
+++ src/main/java/com/example/management/StudentManagementSystemApplication.java
@@ -8,8 +8,6 @@ public class StudentManagementSystemApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(StudentManagementSystemApplication.class, args);
-		
-		//valami miatt nem fut, kitaálni miért!!
 	}
 
 }
