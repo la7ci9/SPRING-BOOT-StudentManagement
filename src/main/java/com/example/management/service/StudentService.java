6de515a13bd4a9c39eedf2ffecfe4baff45936dd
la7ci9 <akarmiis997@gmail.com>
2022-06-17 09:07:57 +0200

    Implement getStudent and updateStudent

--- src/main/java/com/example/management/service/StudentService.java
+++ src/main/java/com/example/management/service/StudentService.java
@@ -27,6 +27,15 @@ public class StudentService implements StudentServiceDAO{
 	public Student saveStudent(Student student) {
 		return studentRepository.save(student);
 	}
-	
-	
+
+	@Override
+	public Student getStudentById(Long id) {
+		return studentRepository.findById(id).get();
+	}
+
+	@Override
+	public Student updateStudent(Student student) {
+		return studentRepository.save(student);
+	}
+
 }
